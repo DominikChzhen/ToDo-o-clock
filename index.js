0452de766f1f5a0d4fb7da321b81d9536b06695a
Konstantin Knorre <Konstantin-oliver@outlook.de>
2025-02-18 17:30:49 +0100

    static frontend code mit ejs verbunden. Server rendert jetzt / und /Calendo

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 const app = express();
 const mongoose = require("mongoose");
-const Subredditt = require("./models/subreddit");
+const User = require("./models/user");
 
+app.use(express.static(path.join(__dirname, '/views')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
@@ -13,9 +14,9 @@ app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, '/views'));
 mongoose.set("strictQuery", true);
 
-mongoose.connect("mongodb://127.0.0.1:27017/redditDB")
+mongoose.connect("mongodb://127.0.0.1:27017/calendoDB")
 .then(() => {
-    console.log("connected");
+    console.log("DB is connected");
 })
 .catch(err => {
     console.log("OH NO ERROR!!");
@@ -24,9 +25,14 @@ mongoose.connect("mongodb://127.0.0.1:27017/redditDB")
 
 //Home
 app.get("/", (req, res) => {
-    res.render("Home");
+    res.render("home");
 })
 
+//Calendo Home
+app.get("/Calendo", (req, res) => {
+    res.render("Calendo");
+})
+/*
 //subreddit Search request
 app.post("/subreddit", (req, res) => {
     let {subreddit} = req.body;
@@ -35,13 +41,12 @@ app.post("/subreddit", (req, res) => {
 
 //new comment post request
 app.post("/subreddit/comment", (req, res) => {
-
-            res.redirect(`http://localhost:8080/r/${currentSubreddit}`)
+            //res.redirect(`http://localhost:8080/r/${currentSubreddit}`)
             //res.render("subreddit", {subreddit, comments: subs.comments})
 })
 
 //pach to edit comment
-
+*/
 app.listen(8080, () => {
-    console.log("listening...");
+    console.log("Server is listening...");
 })
